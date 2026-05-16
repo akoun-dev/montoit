c31d7e4723ef3fd8309b9f88fbf4d49871682c60
Z User <z@container>
2026-05-16 10:29:53 +0000

    75dff03b-af26-45df-991b-067466b708f1

--- keep-alive.sh
+++ keep-alive.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Keep-alive: restart the Next.js server automatically when it dies
-cd /home/z/my-project
 while true; do
-  if ! lsof -ti:3000 >/dev/null 2>&1; then
-    echo "[$(date)] Server dead, restarting..." >> /home/z/my-project/dev.log
-    node_modules/.bin/next dev --port 3000 >> /home/z/my-project/dev.log 2>&1 &
-    sleep 5
-  fi
-  sleep 3
+  sleep 15
+  curl -s -o /dev/null http://localhost:3000/ 2>/dev/null
 done
